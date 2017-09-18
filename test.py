8d01ce50eb81c76c69e1895de61d7616ca160244
vadhavana <32058734+vadhavana@users.noreply.github.com>
2017-09-18 16:05:29 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+#Create List From String
+
+a = "I love Python"
+print(a.split(' '))
+print(a.split('o'))
+
+
+# creAte List to String 
+
+b = ['I','love','python']
+
+print(' '.join(b))
+print('--'.join(b))
